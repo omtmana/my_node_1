f0eaee5e4d0acbaaedbe38d532fca89b646b67ab
Olivia <oliviatmanalastas@gmail.com>
2023-01-16 08:57:01 -0500

    map over arr

--- starter/index.js
+++ starter/index.js
@@ -33,12 +33,27 @@ const url = require('url')
 
 // ***************** SERVER ****************
 // This is how you add the file synchonously
+const replaceTemplate = (temp, product) => {
+   let output = temp.replace(/{%PRODUCTNAME}/g, product.productName)
+   output = output.replace(/{%IMAGE%}/g, product.image)
+   output = output.replace(/{%PRICE%}/g, product.price)
+   output = output.replace(/{%FROM%}/g, product.from)
+   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+   output = output.replace(/{%QUANTITY%}/g, product.quantity)
+   output = output.replace(/{%DESCRIPTION%}/g, product.description)
+   output = output.replace(/{%ID%}/g, product.id)
+
+   if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+
+   return output;
+}
+
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-const dataObj = JSON.parse(data)
+const dataObj = JSON.parse(data) //array of object //loop through this array and replace data 
 
 // Reading Data from API
 const server = http.createServer((req, res) => {
@@ -49,6 +64,9 @@ const server = http.createServer((req, res) => {
    if (path_name === '/' || path_name === '/overview') {
       // load/read template overview
       res.writeHead(200, {'Content-type': 'text/html'})
+
+      const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el))
+
       res.end(tempOverview)
 
    // PRODUCT PAGE
